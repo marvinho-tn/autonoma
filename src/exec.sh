d52674e46c4e9b5d36ae9b7363f8bfdb85de1781
marvinho-tn <marvinthomaz@gmail.com>
2024-04-11 05:23:04 -0300

    requirements

---
+++ src/exec.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Instalar o Python
+sudo apt-get update
+sudo apt-get install python3.9
+
+# Instalar o pip
+sudo apt-get install python3-pip
+
+# Instalar todas as bibliotecas necessárias
+sudo pip3 install -r requirements.txt
+
+# Configurar o ambiente, se necessário
+# Exemplo: Definir variáveis de ambiente, configurar permissões de arquivos, etc.
+
+# Executar o main.py
+python3.9 main.py
